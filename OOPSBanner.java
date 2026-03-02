630e80f9cc2223201b2f7268915c86d25f0cf7fa
sg3949-ux <sg3949@srmist.edu.in>
2026-03-02 14:10:04 +0530

    created oops banner

--- OOPSBanner.java
+++ OOPSBanner.java
@@ -1,18 +1,18 @@
-public class OOPSBannerarrayandloops {
+public class OOPSBanner {
     public static void main(String[] args){
+        String[] banner = {
+            String.join(" ", "   ***   ", "   ***    ", " ******  ", "   *****  "),
+            String.join(" ", " **   ** ", " **   **  ", " *    *   ", " **       "),
+            String.join(" ", "**     **", "**     ** ", " ******   ", "**        "),
+            String.join(" ", "**     **", "**     ** ", " *        ", "  *****   "),
+            String.join(" ", "**     **", "**     ** ", " *        ", "      **  "),
+            String.join(" ", " **   ** ", " **   **  ", " *        ", "**    **  "),
+            String.join(" ", "   ***   ", "   ***    ", " *        ", "  *****   ")
+        };
+        for (String line : banner) {
+            System.out.println(line);
 
-        String[] lines = new String[7];
-
-        lines[0] = String.join(" ", "   *   ", "   *    ", " *** ", "         **** ");
-        lines[1] = String.join(" ", " *   * ", " *   *  ", " *   **   ", "  **       ");
-        lines[2] = String.join(" ", "*     *", "*     * ", " ***  ", "     **        ");
-        lines[3] = String.join(" ", "*     *", "*     * ", " *        ", "   ****  ");
-        lines[4] = String.join(" ", "*     *", "*     * ", " *        ", "       **  ");
-        lines[5] = String.join(" ", " *   * ", " *   *  ", " *        ", "      ** ");
-        lines[6] = String.join(" ", "   *   ", "   *    ", " *        ", "  ****   ");
-
-        for (int i = 0; i < lines.length; i++) {
-            System.out.println(lines[i]);
+            
             
         }
     }
